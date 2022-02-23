353b8d04b74cb7286c171f51cdbb156a4d6de99c
= <naelson.g.saraiva@gmail.com>
2022-02-23 16:59:05 -0400

    Up

---
+++ mysql.py
@@ -0,0 +1,23 @@
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='Admin123.',
+                             database='python',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+with connection:
+    
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `id` FROM `users`"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        print(result)
