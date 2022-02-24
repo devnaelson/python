3b3a200eba55b66aef3ccfa35e45a69e2c5d10ac
= <naelson.g.saraiva@gmail.com>
2022-02-24 14:53:32 -0400

    U

--- lang/class.py
+++ lang/class.py
@@ -57,3 +57,15 @@ class MyClass:
 myobjectx = MyClass()
 
 myobjectx.function()
+
+
+class NumberHolder:
+
+   def __init__(self, number):
+       self.number = number
+
+   def returnNumber(self):
+       return self.number
+
+var = NumberHolder(7)
+print(var.returnNumber()) #Prints '7'
