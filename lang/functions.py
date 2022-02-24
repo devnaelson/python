9e35186ff9d7201ac8902987dac70103b7c20e86
= <naelson.g.saraiva@gmail.com>
2022-02-24 14:21:52 -0400

    Up

---
+++ lang/functions.py
@@ -0,0 +1,12 @@
+def my_function():
+    print("Hello From My Function!")
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
+
+my_function_with_args("Naelson","beat")
+
+def sum_two_numbers(a, b):
+    return a + b
+
+print(" %d ",sum_two_numbers("10","10"))
